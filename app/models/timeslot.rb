616a37df0e3957d2476366366ecaa285dd1dd23a
Cathy Douglass <cdouglass256@gmail.com>
2017-09-23 15:29:21 -0700

    Timeslots: #index includes timeslots overlapping given date

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -13,8 +13,8 @@ class Timeslot < ApplicationRecord
   def Timeslot.all_on_date(date)
     # convert to time so #midnight considers time zone
     date_start = date.to_time.midnight.to_i
-    date_end = (date + 1.day).to_time.midnight.to_i
-    Timeslot.where(start_time: date_start...date_end)
+    date_end = (date + 1.day).to_time.midnight.to_i - 1
+    Timeslot.where("(start_time BETWEEN :date_start AND :date_end) OR (end_time BETWEEN :date_start_exclusive AND :date_end) OR (start_time < :date_start AND end_time > :date_end)", {date_start: date_start, date_end: date_end, date_start_exclusive: date_start + 1})
   end
 
 end
