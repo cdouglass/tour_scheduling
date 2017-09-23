91c6bab4fbb2d03716377df88de005d44061821c
Cathy Douglass <cdouglass256@gmail.com>
2017-09-22 19:45:59 -0700

    Test content of boats controller response

--- test/controllers/api/boats_controller_test.rb
+++ test/controllers/api/boats_controller_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
 class Api::BoatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   def test_gets_index
     get api_boats_url
     assert_response :success
+    expected_response = "[{\"id\":298486374,\"name\":\"MyString\",\"capacity\":1},{\"id\":980190962,\"name\":\"MyString\",\"capacity\":1}]"
+    assert_equal(response.body, expected_response)
   end
 
   def test_create
     assert_difference('Boat.count', 1) do
       post api_boats_url, params: {boat: {name: "Zodiac", capacity: "2"}}
+      assert_response :success
+      expected_response = "{\"id\":980190963,\"name\":\"Zodiac\",\"capacity\":2}"
+      assert_equal(response.body, expected_response)
     end
   end
 end
