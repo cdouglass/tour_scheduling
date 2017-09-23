b5b149f951e9f32dfdeaf5b8d75aea4888ff8dd1
Cathy Douglass <cdouglass256@gmail.com>
2017-09-22 17:45:41 -0700

    Test Boats controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :boats, only: [:index, :create]
+    defaults format: :json do
+      resources :boats, only: [:index, :create]
+    end
   end
 
 end
--- test/controllers/api/boats_controller_test.rb
+++ test/controllers/api/boats_controller_test.rb
@@ -4,4 +4,14 @@ class Api::BoatsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def test_gets_index
+    get api_boats_url
+    assert_response :success
+  end
+
+  def test_create
+    assert_difference('Boat.count', 1) do
+      post api_boats_url, params: {boat: {name: "Zodiac", capacity: "2"}}
+    end
+  end
 end
