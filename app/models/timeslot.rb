b00b4d63ffdc2b8bcb73e241557f656279837e98
Cathy Douglass <cdouglass256@gmail.com>
2017-09-23 16:58:21 -0700

    Timeslots: validate duration is positive

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,5 +1,7 @@
 class Timeslot < ApplicationRecord
 
+  validate :end_after_start
+
   def duration
     (self.end_time - self.start_time) / 60
   end
@@ -15,6 +17,14 @@ class Timeslot < ApplicationRecord
     date_start = date.to_time.midnight.to_i
     date_end = (date + 1.day).to_time.midnight.to_i - 1
     Timeslot.where("(start_time BETWEEN :date_start AND :date_end) OR (end_time BETWEEN :date_start_exclusive AND :date_end) OR (start_time < :date_start AND end_time > :date_end)", {date_start: date_start, date_end: date_end, date_start_exclusive: date_start + 1})
+  end
+
+  private
+
+  def end_after_start
+    if self.end_time <= self.start_time
+      errors.add(:end_time, "Duration must be a positive number")
+    end
   end
 
 end
--- test/models/timeslot_test.rb
+++ test/models/timeslot_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TimeslotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @timeslot = Timeslot.new(start_time: 1_000_000, end_time: 2_000_000)
+  end
+
+  def test_valid_timeslot
+    assert @timeslot.valid?
+  end
+
+  def test_validates_end_after_start
+    @timeslot.end_time = @timeslot.start_time
+    assert !@timeslot.valid?
+
+    @timeslot.end_time = @timeslot.start_time - 1_000
+    assert !@timeslot.valid?
+  end
 end
