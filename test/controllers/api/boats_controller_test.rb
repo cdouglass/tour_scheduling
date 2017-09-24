548ad234e4ef469f79269a44d4ec551ce84ded3d
Cathy Douglass <cdouglass256@gmail.com>
2017-09-24 13:32:59 -0700

    Name boat fixtures

--- test/controllers/api/boats_controller_test.rb
+++ test/controllers/api/boats_controller_test.rb
@@ -4,7 +4,7 @@ class Api::BoatsControllerTest < ActionDispatch::IntegrationTest
   def test_gets_index
     get api_boats_url
     assert_response :success
-    expected_response = "[{\"id\":298486374,\"name\":\"MyString\",\"capacity\":1},{\"id\":980190962,\"name\":\"MyString\",\"capacity\":1}]"
+    expected_response = "[{\"id\":1,\"name\":\"Another Fine Product From The Nonsense Factory\",\"capacity\":15},{\"id\":2,\"name\":\"Just Read The Instructions\",\"capacity\":40},{\"id\":3,\"name\":\"Just Testing\",\"capacity\":10},{\"id\":4,\"name\":\"Funny, It Worked Last Time...\",\"capacity\":1000000}]"
     assert_equal(response.body, expected_response)
   end
 
@@ -12,7 +12,7 @@ class Api::BoatsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Boat.count', 1) do
       post api_boats_url, params: {boat: {name: "Zodiac", capacity: "2"}}
       assert_response :success
-      expected_response = "{\"id\":980190963,\"name\":\"Zodiac\",\"capacity\":2}"
+      expected_response = "{\"id\":5,\"name\":\"Zodiac\",\"capacity\":2}"
       assert_equal(response.body, expected_response)
     end
   end
