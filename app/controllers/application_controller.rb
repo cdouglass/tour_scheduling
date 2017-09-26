b530a5b52a8d1eddb07306de33dc555c7086cbc9
Cathy Douglass <cdouglass256@gmail.com>
2017-09-25 22:50:58 -0700

    Allow use from browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+
+  after_action :set_access_control_headers
+
+  private
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "http://localhost:3333"
+    headers['Access-Control-Allow-Headers'] = "*"
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
